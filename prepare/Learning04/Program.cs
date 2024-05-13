4c735ecc662868ff2c14744d505fa429df59fa1c
Fierilyheavenlyeagle <alvarocerpabaron0502@gmail.com>
2024-05-13 13:47:20 -0500

    sync changes

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -5,5 +5,17 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning04 World!");
+
+        Assignment assignment = new Assignment("Rodgrigo", "History");
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment assignment1 = new MathAssignment("Diego", "Fractions", "Section 7.3", "Problems 8-19");
+        Console.WriteLine(assignment1.GetSummary());
+        Console.WriteLine(assignment1.GetHomeworkList());
+        
+        WritingAssignment assignment2 = new WritingAssignment("Carla Moscozo","Biology","The hybridization of Robots with Biology");
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetWritingInformation());
+
     }
 }
